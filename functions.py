4b9653e20a02e3a7f45e212bba233086f1790f44
Marco Bonato <marcobonato09@gmail.com>
2023-11-13 11:48:29 +0100

    Update functions.py
    Reformatted code slightly for better readability and added use of epsilon to prevent divisions by zero

--- functions.py
+++ functions.py
@@ -6,18 +6,23 @@ class Function:
         self.derivative_function = derivative_function
         
         
-    def __call__(self, X, *args):
-        return self.function(X, *args)
+    def __call__(self, *args):
+        return self.function(*args)
     
     
-    def derivative(self, X, *args):
-        return self.derivative_function(X, *args)
+    def derivative(self, *args):
+        return self.derivative_function(*args)
         
+epsilon = 10**-7
         
-sigmoid = Function(lambda X: 1/(1+np.e**-X), lambda X: 1/(1+np.e**-X)*(1-1/(1+np.e**-X)))
-relu = Function(lambda X: np.maximum(0, X), lambda X: X>0)
-softmax = Function(lambda X: np.exp(X)/sum(np.exp(X)), lambda X: np.exp(X)/sum(np.exp(X))*(1-np.exp(X)/sum(np.exp(X))))
-binary_cross_entropy = Function(lambda a, Y: Y*np.log(a)+(1-Y)*np.log(1-a), lambda a, Y: -Y/a+(1-Y)/(1-a))
-mean_squared_error = Function(lambda a, Y: (a-Y)**2, lambda a, Y: 2*(a-Y))
-
+sigmoid = Function(lambda X: 1/(1+np.e**-X), 
+                   lambda X: 1/(1+np.e**-X)*(1-1/(1+np.e**-X)))
+relu = Function(lambda X: np.maximum(0, X), 
+                lambda X: X>0)
+softmax = Function(lambda X: np.exp(X)/(sum(np.exp(X))+epsilon), 
+                   lambda X: (np.exp(X)/(sum(np.exp(X))+epsilon))*(1-(np.exp(X)/(sum(np.exp(X))+epsilon))))
+binary_cross_entropy = Function(lambda a, Y: Y*np.log(a)+(1-Y)*np.log(1-a), 
+                                lambda a, Y: -Y/(a+epsilon)+(1-Y)/(1-a+epsilon))
+mean_squared_error = Function(lambda a, Y: (a-Y)**2, 
+                              lambda a, Y: 2*(a-Y))
 
